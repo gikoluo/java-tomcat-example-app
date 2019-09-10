3af53f9e17eec6ab4bf355ca299923bb1f9fe85f
Luo Chunhui <luochunhui@greenlandfinancial.com>
2019-09-10 11:38:36 +0800

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,97 +1,73 @@
-#!groovy
+#!groovy 
 
-@Library('luochunhui')
-import com.luochunhui.lib.Utilities
-import com.luochunhui.lib.Remote
-
-def utils = new Utilities(steps)
-
-targetFile="target/hellowar.war"
-playbook= "samples/hellowar"
-remoteUser=""
-test_url = ""
-uat_url = ""
-prod_url = ""
+// Jenkinsfile
+def projectName = env.PROJECT_NAME   //Project name, Usually it is the name of jenkins project folder name.
+def serviceName = env.SERVICE_NAME   //Service name. Usually it is the process name running in the server.
+def branchName = env.BRANCH_NAME     //Branch name. And the project must be multibranch pipeline, Or set the env in config
 
 
-milestone 1
-stage('Dev') {
-    node {
-        checkout scm
-        utils.maven 'compile war:war'
-
-        dir(".") {
-            archiveArtifacts artifacts:targetFile, fingerprint: true
-            stash name:'targetArchive', includes:targetFile
-        }
+pipeline {
+  agent {
+    kubernetes {
+      // this label will be the prefix of the generated pod's name
+      //label '${projectName}-${serviceName}'
+      defaultContainer 'jnlp'
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    component: ci
+spec:
+  containers:
+  - name: jnlp
+    image: 'jenkins/jnlp-slave:3.27-1-alpine'
+    args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
+  - name: docker
+    image: docker
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+      - mountPath: /var/run/docker.sock
+        name: docker-sock
+  - name: maven
+    image: maven:3-jdk-8
+    command:
+    - cat
+    tty: true
+  - name: kubectl
+    image: lachlanevenson/k8s-kubectl:v1.14.0 // use a version that matches your K8s version
+    command:
+      - cat
+    tty: true
+  volumes:
+    - name: docker-sock
+      hostPath:
+        path: /var/run/docker.sock
+"""
     }
-}
+  }
 
-
-/*
-milestone 2
-stage('Quick Test') {
-    node {
-        deploy( targetFile, playbook, 'uat' )
+  stages {
+    stage('Build image') {
+      steps {
+        container('docker') {
+          sh "docker build -t ${projectName}-${serviceName}:${branchName} ."
+          sh "docker push ${projectName}-${serviceName}:${branchName}"
+        }
+      }
     }
-}
-*/
 
-
-// NO test enviroment now
-// milestone 2
-// stage('Deploy Test') {
-//     node {
-//         remote = new Remote(steps, 'test', remoteUser)
-//         remote.deployAnsible( targetFile )
-//     }
-// }
-
-milestone 3
-stage('UAT') {
-    lock(resource: "${playbook}-UAT", inversePrecedence: true) {
-        milestone 3
-        timeout(time:1, unit:'DAYS') {
-            input id: 'pushToUAT', message: "Test环境正常了么？可以提交 UAT 了吗?", ok: '准备好了，发布！', submitter: 'sa'
+    stage('Deploy') {
+      steps {
+        container('kubectl') {
+            sh "kubectl version"
+          // sh "kubectl delete -f ./kubernetes/deployment.yaml"
+          // sh "kubectl apply -f ./kubernetes/deployment.yaml"
+          // sh "kubectl apply -f ./kubernetes/service.yaml"
         }
-
-        node {
-            echo 'UAT deploy start'
-            remote = new Remote(steps, 'uat', remoteUser)
-            remote.deployAnsible( targetFile )
-            echo "UAT deployed"
-        }
-        
-        timeout(time:1, unit:'DAYS') {
-            input message: " UAT 通过了吗? ${uat_url} ", ok: '通过！', submitter: 'sa'
-        }
+      }
     }
+  }
 }
-
-
-milestone 4
-stage ('Production') {
-    lock(resource: "${playbook}-Production", inversePrecedence: true) {
-        timeout(time:1, unit:'DAYS') {
-            input message: "可以提交 Prod 了吗?", ok: '准备好了，发布！', submitter: 'sa'
-        }
-        
-        node {
-            echo 'Production deploy status'
-            remote = new Remote(steps, 'prod', remoteUser)
-            remote.deployAnsible( targetFile )
-            echo "Production deployed"
-        }
-        
-        timeout(time:1, unit:'DAYS') {
-            input message: "Prod测试完成了吗? ${prod_url} ", ok: '通过！下班，困觉！', submitter: 'sa'
-        }
-    }
-}
-
-
-
-
-
-
-
