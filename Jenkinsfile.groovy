1fd48f6f551f3257a4e2185c8915dbf5b659653c
Luo Chunhui <luochunhui@greenlandfinancial.com>
2019-09-16 15:29:17 +0800

    sonar

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -134,10 +134,11 @@ spec:
               def image = docker.build("${tag}:sonarqube", "--target build_stage .")
               image.inside {  //docker inside changed the workdir to project home. so cd /build is required
                 sh "pwd"
-                sh """
-                cat sonar-project.properties;
-                sonar-scanner -Dsonar.host.url=http://docker.for.mac.host.internal:9000 || echo 'Snoar scanner failed';
-                """
+                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar -Dsonar.host.url=http://docker.for.mac.host.internal:9000'
+                //sh """
+                //cat sonar-project.properties;
+                //sonar-scanner -Dsonar.host.url=http://docker.for.mac.host.internal:9000 || echo 'Snoar scanner failed';
+                //"""
               }
             }
             else {
