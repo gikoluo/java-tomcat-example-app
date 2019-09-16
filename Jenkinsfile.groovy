83e63edafdb6ea40262d60f92ca428d7ee9710fd
Luo Chunhui <luochunhui@greenlandfinancial.com>
2019-09-16 15:20:32 +0800

    sonar

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -118,7 +118,9 @@ spec:
               def scannerHome = tool 'SonarScanner 4.0';
               withSonarQubeEnv('SonarQubeServer') { // If you have configured more than one global server connection, you can specify its name
                 //sh "${scannerHome}/bin/sonar-scanner"
-                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar'
+                withMaven(maven:'Maven 3.5') {
+                      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar'
+                  }
               }
 
               // image.inside {
