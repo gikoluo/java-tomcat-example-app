152ee02cc41cff12e300043a95b924257d4692ed
Luo Chunhui <luochunhui@greenlandfinancial.com>
2019-09-16 11:53:06 +0800

    sonar ip

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -123,9 +123,9 @@ spec:
               """
               def image = docker.image("${tag}:sonarqube")
               image.inside() {
-                sh "curl http://devops-sonarqube-sonarqube:9000/ || echo curl devops-sonarqube-sonarqube"
-                sh "curl http://sonarqube:9000/ || echo curl sonarqube"
-                sh "curl http://devops-sonarqube-sonarqube.devops.svc.cluster.local:9000/ || echo curl sonarqube"
+                // sh "curl http://docker.for.mac.host.internal:9000/ || echo curl devops-sonarqube-sonarqube"
+                // sh "curl http://sonarqube:9000/ || echo curl sonarqube"
+                // sh "curl http://devops-sonarqube-sonarqube.devops.svc.cluster.local:9000/ || echo curl sonarqube"
                 sh "sonar-scanner || echo 'Snoar scanner failed' "
               }
             }
