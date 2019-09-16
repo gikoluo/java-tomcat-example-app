3495982da33a6d8e0ca7cddf662662678b9391df
Luo Chunhui <luochunhui@greenlandfinancial.com>
2019-09-16 14:11:48 +0800

    sonar

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -106,18 +106,19 @@ spec:
       }
     }
 
-    stage('QA') {
+    stage('SonarQube analysis') {
       steps {
         container('docker') {
-          echo "Run Sonar Analytics"
+          echo "Run SonarQube Analysis"
           script {
             if(! skipQA) {
               // def image = docker.image("newtmitch/sonar-scanner:4").withRun(){
               //   sh "sonar-scanner -Dsonar.host.url=http://docker.for.mac.host.internal:9000 || echo 'Snoar scanner failed';"
               // }
-              def scannerHome = tool 'SonarScanner 4.0';
+              def scannerHome = tool 'SonarScanner';
               withSonarQubeEnv('SonarQubeServer') { // If you have configured more than one global server connection, you can specify its name
-                sh "${scannerHome}/bin/sonar-scanner"
+                //sh "${scannerHome}/bin/sonar-scanner"
+                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.6.0.1398:sonar'
               }
 
               // image.inside {
